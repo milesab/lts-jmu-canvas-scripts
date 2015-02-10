05b920b2f5395cf3924035f903cba027f32b6996
Jon Short <shortjw@jmu.edu>
2015-02-10 15:31:39 -0500

    Added instructor_courses python data feed

---
+++ apps/feeds/instructor_courses.py
@@ -0,0 +1,29 @@
+import os, sys, re, cgi
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'bin')))
+import api_local
+config = api_local.get_config()
+
+if __name__ == '__main__':
+
+
+    teacher_ids = "balltc"
+    #teacher_ids = cgi.FieldStorage().getvalue('id')
+    teachers = teacher_ids.split(',')
+
+    output = []
+
+    enrollments = api_local.read_csv(config['export_dir'] + 'enrollments.csv', 'course_id')
+    
+    for teacher in teachers:
+        output.append("Teacher: %s\ncourse_id (canvas_course_id)\tsection_id (canvas_section_id)" % teacher)
+        for enrollment in enrollments:
+            if enrollment['role'] == "teacher" and enrollment['user_id'] == teacher:
+                cid = enrollment['course_id']
+                ccid = enrollment['canvas_course_id']
+                sid = enrollment['section_id']
+                csid = enrollment['canvas_section_id']
+                output.append("%s (%s)\t%s (%s)" % (cid,ccid,sid,csid))
+
+    print "Content-type: text/plain\n";
+    for line in output:
+        print line
