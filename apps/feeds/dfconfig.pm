9603d9c4ceb71052fa1b939fa9ca817ee1e0f692
Jon Short <shortjw@jmu.edu>
2015-02-11 15:04:55 -0500

    Removed legacy perl feeds

--- apps/feeds/dfconfig.pm
+++
@@ -1,9 +0,0 @@
-package DFconfig;
-use strict;
-
-open my $cf, '<', '../../conf/settings.conf' or die "Unable to open file:$!\n";
-my %config = map { s/#.*//; s/^\s+//; s/\s+$//; split /=|\s+/; } <$cf>;
-close $cf;
-
-sub get_config { return %config; }
-1;
