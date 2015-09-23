126d2cbe9b7e659a07a195d9bdf0fed3feacad2e
Jon Short <shortjw@jmu.edu>
2015-09-23 14:20:37 -0400

    Variables renamed to be internally consistent.

--- apps/feeds/instructor_courses.py
+++ apps/feeds/instructor_courses.py
@@ -8,14 +8,14 @@ export_dir = config['local']['export_dir']
 
 if __name__ == '__main__':
 
-    teacher_ids = cgi.FieldStorage().getvalue('id')
-    teachers = teacher_ids.split(',')
+    instructor_ids = cgi.FieldStorage().getvalue('id')
+    instructors = instructor_ids.split(',')
     enrollments = api_local.read_csv(export_dir + 'enrollments.csv', 'course_id')
 
     output = []
     
-    for teacher in teachers:
-        output.append("\nTeacher: %s\n\ncourse_id (canvas_course_id)\tsection_id (canvas_section_id)" % teacher)
+    for instructor in instructors:
+        output.append("\nInstructor: %s\n\ncourse_id (canvas_course_id)\tsection_id (canvas_section_id)" % teacher)
         for enrollment in enrollments:
             if enrollment['role'] == "teacher" and enrollment['user_id'] == teacher:
                 cid = enrollment['course_id']
