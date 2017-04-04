761ed1265f4da94821d95145360ec6223619dbf6
shortjw <shortjw@jmu.edu>
2017-04-04 17:27:59 -0400

    import dependencies and timestamp

--- bin/mpe_enrollment.py
+++ bin/mpe_enrollment.py
@@ -1,9 +1,12 @@
 import api_local, api_canvas
+import shutil, os
+from datetime import datetime
 
 config = api_local.get_config()
 easel_home = config['local']['easel_home']
 import_file = config['local']['import_dir'] + 'math_placement/mathp_enrollment.csv'
 import_id_file = easel_home + 'data/temp/mpe_importid.txt'
+timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
 
 
 if __name__ == '__main__':
@@ -12,4 +15,4 @@ if __name__ == '__main__':
     if os.stat(import_file).st_size > 0:
         shutil.copy2(import_file,easel_home + 'data/mpe/enrollments/mathp_enrollment-%s.csv' % timestamp)
 
-    api_canvas.import_submit(import_file,import_id_file)
+    #api_canvas.import_submit(import_file,import_id_file)
